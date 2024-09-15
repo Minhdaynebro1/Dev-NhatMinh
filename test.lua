0cc8c73aa6c52b71f08514e38b69a749579c9253
Minhdaynebro1 <164420001+Minhdaynebro1@users.noreply.github.com>
2024-09-15 10:41:55 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -4,6 +4,8 @@ elseif game.PlaceId == 5902977746 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/main/Ultimate%20Tower%20Defense.lua"))();
 elseif game.PlaceId == 16480898254 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/main/Eat%20The%20World.lua"))();
+elseif game.PlaceId == 16146832113 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/main/Anime%20Vanguard.lua"))();
 else
-  warn("Game is not supported!");
+  game.Players.LocalPlayer:Kick("Game is not supported!");
 end;
