371b22e4311126e67e007fd10b6519f6d7e4987c
Minhdaynebro1 <164420001+Minhdaynebro1@users.noreply.github.com>
2024-12-15 05:55:48 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -8,6 +8,8 @@ elseif game.PlaceId == 3101667897 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/Legends%20of%20Speed.lua"))();
 elseif game.PlaceId == 13827198708 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/Pull%20A%20Sword.lua"))();
+elseif game.PlaceId == 116605585218149 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/Go%20Fishing.lua"))();
 else
   game.Players.LocalPlayer:Kick("Game is not supported!");
 end;
