acd7a834e22c9332c2b22d1a58c1b1a16c9ffb68
Minhdaynebro1 <164420001+Minhdaynebro1@users.noreply.github.com>
2025-01-04 17:51:59 -0600

    Update test.lua

--- test.lua
+++ test.lua
@@ -4,10 +4,6 @@ elseif game.PlaceId == 5902977746 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/main/Ultimate%20Tower%20Defense.lua"))();
 elseif game.PlaceId == 16480898254 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/main/Eat%20The%20World.lua"))();
-elseif game.PlaceId == 3101667897 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/Legends%20of%20Speed.lua"))();
-elseif game.PlaceId == 13827198708 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/Pull%20A%20Sword.lua"))();
 elseif game.PlaceId == 116605585218149 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/Go%20Fishing.lua"))();
 else
