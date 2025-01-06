7e0d25bd2ba3a14edc70b56788b84dd2960e2efc
Minhdaynebro1 <164420001+Minhdaynebro1@users.noreply.github.com>
2025-01-06 16:24:18 -0600

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,23 +1,19 @@
-if game.PlaceId == 15705682243 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/main/Tapping%20Legends%20Final.lua"))();
-elseif game.PlaceId == 5902977746 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/main/Ultimate%20Tower%20Defense.lua"))();
-elseif game.PlaceId == 16480898254 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/main/Eat%20The%20World.lua"))();
-elseif game.PlaceId == 116605585218149 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/Go%20Fishing.lua"))();
-elseif game.PlaceId == 8304191830 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/Anime%20AdventureS.lua"))();
-elseif game.PlaceId == 8349889591 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/Anime%20AdventureS.lua"))();
-elseif game.PlaceId == 14229762361 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/Anime%20AdventureS.lua"))();
-elseif game.PlaceId == 14918509670 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/Anime%20AdventureS.lua"))();
-elseif game.PlaceId == 14229839966 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/Anime%20AdventureS.lua"))();
-elseif game.PlaceId == 3183403065 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/Anime%20AdventureS.lua"))();
+local placeId = game.PlaceId
+local scripts = {
+    [15705682243] = "https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/main/Tapping%20Legends%20Final.lua",
+    [5902977746]  = "https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/main/Ultimate%20Tower%20Defense.lua",
+    [16480898254] = "https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/main/Eat%20The%20World.lua",
+    [116605585218149] = "https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/Go%20Fishing.lua",
+    [8304191830]  = "https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/Anime%20AdventureS.lua",
+    [8349889591]  = "https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/Anime%20AdventureS.lua",
+    [14229762361] = "https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/Anime%20AdventureS.lua",
+    [14918509670] = "https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/Anime%20AdventureS.lua",
+    [14229839966] = "https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/Anime%20AdventureS.lua",
+    [3183403065]  = "https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/Anime%20AdventureS.lua"
+}
+
+if scripts[placeId] then
+    loadstring(game:HttpGet(scripts[placeId]))()
 else
-  game.Players.LocalPlayer:Kick("Game is not supported!");
-end;
+    game.Players.LocalPlayer:Kick("Game is not supported!")
+end
