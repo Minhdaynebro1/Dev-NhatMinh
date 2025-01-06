0f2670e7f560af65124915ea1629aa1f616cc803
Minhdaynebro1 <164420001+Minhdaynebro1@users.noreply.github.com>
2025-01-06 16:03:33 -0600

    Update test.lua

--- test.lua
+++ test.lua
@@ -6,6 +6,8 @@ elseif game.PlaceId == 16480898254 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/main/Eat%20The%20World.lua"))();
 elseif game.PlaceId == 116605585218149 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/Go%20Fishing.lua"))();
+elseif game.PlaceId == 8304191830 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/Anime%20AdventureS.lua"))();
 else
   game.Players.LocalPlayer:Kick("Game is not supported!");
 end;
