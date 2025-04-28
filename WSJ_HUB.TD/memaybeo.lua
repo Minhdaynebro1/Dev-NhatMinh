a6fc2138c635324173b0202ae9785289e6713bae
Minhdaynebro1 <164420001+Minhdaynebro1@users.noreply.github.com>
2025-04-28 18:08:44 -0500

    Update memaybeo.lua

--- WSJ_HUB.TD/memaybeo.lua
+++ WSJ_HUB.TD/memaybeo.lua
@@ -1,1 +1,17 @@
-.
+local placeId = game.PlaceId
+local scripts = {
+    [5902977746]  = "https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/main/Ultimate%20Tower%20Defense.lua",
+    [8304191830]  = "https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/Anime%20AdventureS.lua",
+    [8349889591]  = "https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/Anime%20AdventureS.lua",
+    [14229762361] = "https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/Anime%20AdventureS.lua",
+    [14918509670] = "https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/Anime%20AdventureS.lua",
+    [14229839966] = "https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/Anime%20AdventureS.lua",
+    [3183403065]  = "https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/Anime%20AdventureS.lua",
+    [72829404259339]  = "https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/Anime%20Rangers%20X.lua"
+}
+
+if scripts[placeId] then
+    loadstring(game:HttpGet(scripts[placeId]))()
+else
+    game.Players.LocalPlayer:Kick("Game is not supported!")
+end
