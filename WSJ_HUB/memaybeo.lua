5d1abd16ffece52d4d0035b315f01666b9120ed0
Minhdaynebro1 <164420001+Minhdaynebro1@users.noreply.github.com>
2025-04-30 06:21:27 -0500

    Update memaybeo.lua

--- WSJ_HUB/memaybeo.lua
+++ WSJ_HUB/memaybeo.lua
@@ -1,6 +1,5 @@
 local placeId = game.PlaceId
 local scripts = {
-    [15705682243] = "https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/WSJ_HUB/Tapping%20Legends%20Final.lua",
     [16480898254] = "https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/WSJ_HUB/Eat%20The%20World.lua",
     [116605585218149] = "https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/WSJ_HUB/Go%20Fishing%20W1.lua",
     [106962503558742] = "https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/WSJ_HUB/Go%20Fishing%20W2.lua",
