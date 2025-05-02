2e56bcdffd6c8e34f61efdb59afb6522e7ec1e9e
Minhdaynebro1 <164420001+Minhdaynebro1@users.noreply.github.com>
2025-05-03 06:42:24 +0800

    Update ValidateKey.lua

--- WSJ_HUB.PREMIUM/ValidateKey.lua
+++ WSJ_HUB.PREMIUM/ValidateKey.lua
@@ -1,27 +1,16 @@
-keyListUrl = "https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/WSJ_HUB.PREMIUM/KEY-PREMIUM"
-
-local lastValidTime = nil
+keyListUrl = "[https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/WSJ\_HUB.PREMIUM/KEY-PREMIUM](https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/WSJ_HUB.PREMIUM/KEY-PREMIUM)"
 
 function isValidKey(key)
-    local currentTime = tick()
+    local success, response = pcall(function()
+        return game\:HttpGet(keyListUrl)
+    end)
 
-    -- Nếu đã có thời gian hợp lệ và đã quá 60 giây thì từ chối
-    if lastValidTime and (currentTime - lastValidTime > 60) then
-        return false
-    end
-
-    local success, response = pcall(function()
-        return game:HttpGet(keyListUrl)
-    end)
-
-    if success then
-        for _, v in ipairs(response:split("\n")) do
-            if key == v then
-                lastValidTime = currentTime -- Ghi nhận thời gian dùng key thành công
-                return true
-            end
-        end
-    end
-
-    return false
+    if success then
+        for \_, v in ipairs(response\:split("\n")) do
+            if key == v then
+                return true
+            end
+        end
+    end
+    return false
 end
