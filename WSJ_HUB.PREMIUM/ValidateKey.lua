caf41315a9f66038aa65cefba715316074a4492a
Minhdaynebro1 <164420001+Minhdaynebro1@users.noreply.github.com>
2025-05-03 06:04:10 +0800

    Update ValidateKey.lua

--- WSJ_HUB.PREMIUM/ValidateKey.lua
+++ WSJ_HUB.PREMIUM/ValidateKey.lua
@@ -1,1 +1,18 @@
+-- ValidateKey.lua - chỉ dùng để kiểm tra key
 
+keyListUrl = "https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/WSJ_HUB.PREMIUM/KEY-PREMIUM"
+
+function isValidKey(key)
+    local success, response = pcall(function()
+        return game:HttpGet(keyListUrl)
+    end)
+
+    if success then
+        for _, v in ipairs(response:split("\n")) do
+            if key == v then
+                return true
+            end
+        end
+    end
+    return false
+end
