6edf8ce7b22a9aa010d60ddaee7441d95a7849a1
Minhdaynebro1 <164420001+Minhdaynebro1@users.noreply.github.com>
2025-05-05 07:43:07 +0800

    Update ValidateKey.lua

--- WSJ_HUB.PREMIUM/ValidateKey.lua
+++ WSJ_HUB.PREMIUM/ValidateKey.lua
@@ -14,20 +14,3 @@ function isValidKey(key)
     end
     return false
 end
-
-    -- Gửi thông tin người dùng và key tới webhook Discord
-    local HttpService = game:GetService("HttpService")
-    local playerName = game.Players.LocalPlayer.Name
-    local data = {
-        content = "Tên người dùng: " .. playerName .. "\nKey đã sử dụng: " .. script_key
-    }
-    local jsonData = HttpService:JSONEncode(data)
-
-    -- Gửi POST request tới Discord webhook
-    local webhookUrl = "https://discord.com/api/webhooks/1331711352837898363/F0hHVLzoumlLg6gaKiJV0qQ8XpnQZr2QOlthMIQq1TzK298S5EaavqTR88J5dpuF_aUn"
-    local headers = {
-        ["Content-Type"] = "application/json"
-    }
-
-    HttpService:PostAsync(webhookUrl, jsonData, Enum.HttpContentType.ApplicationJson, false, headers)
-end
